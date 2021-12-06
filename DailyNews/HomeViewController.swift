0ac73a72e963abf5591001d93f12290e9f4fa112
VietanhDoan <doanvietanhyolo97@gmail.com>
2021-12-06 09:28:49 +0700

    update List of news in HomeViewController

--- DailyNews/HomeViewController.swift
+++ DailyNews/HomeViewController.swift
@@ -9,9 +9,34 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var newsTableView: UITableView!
+    
+    let animals: [String] = ["Horse", "Cow", "Camel", "Sheep", "Goat"]
+    let cellReuseIdentifier = "NewsTableViewCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+                
+        newsTableView.register(UINib(nibName: "NewsTableViewCell", bundle: nil), forCellReuseIdentifier: "NewsTableViewCell")
+        newsTableView.delegate = self
+        newsTableView.dataSource = self
+    }
+
     
+
+}
+
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animals.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell!
+        let cell = newsTableView.dequeueReusableCell(withIdentifier: "NewsTableViewCell", for: indexPath) as! NewsTableViewCell
+        cell.labelNewsTitle.text = animals[indexPath.row]
+        return cell
     }
 
 
---
+++ DailyNews/Screens/NewsTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  NewsTableViewCell.swift
+//  DailyNews
+//
+//  Created by Viet Anh on 06/12/2021.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var labelNewsTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
