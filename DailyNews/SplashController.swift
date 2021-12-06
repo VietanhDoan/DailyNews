2b4140778f05d20b7531584874038c236357d7c0
VietanhDoan <doanvietanhyolo97@gmail.com>
2021-12-06 09:08:43 +0700

    update

--- DailyNews/SplashController.swift
+++ DailyNews/SplashController.swift
@@ -13,10 +13,15 @@ class SplashController: UIViewController {
         super.viewDidLoad()
         
         // Open Home screen
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
             let homeVC = self.storyboard?.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
             self.navigationController?.pushViewController(homeVC, animated: true)
         }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
     }
 
 }
