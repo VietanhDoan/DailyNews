d26f37e04c5233891f81c9f64f71b68054d7f960
VietanhDoan <doanvietanhyolo97@gmail.com>
2022-01-02 19:23:54 +0700

    update

--- DailyNews/Screens/NewsCategoriesController.swift
+++ DailyNews/Screens/NewsCategoriesController.swift
@@ -9,9 +9,31 @@ import UIKit
 
 class NewsCategoriesController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var searchButton: UIImageView!
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+            searchButton.isUserInteractionEnabled = true
+            searchButton.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
+    {
+//        let tappedImage = tapGestureRecognizer.view as! UIImageView
+        let searchVC = self.storyboard?.instantiateViewController(withIdentifier: "SearchController") as! SearchController
+        self.navigationController?.pushViewController(searchVC, animated: true)
+        // Your action
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
 }
---
+++ DailyNews/Screens/SearchController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchController.swift
+//  DailyNews
+//
+//  Created by Viet Anh on 02/01/2022.
+//
+
+import UIKit
+
+class SearchController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
