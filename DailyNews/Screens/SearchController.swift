e3a22c5ee9729afa7234c288010281f2fb02be6b
VietanhDoan <doanvietanhyolo97@gmail.com>
2022-01-03 11:36:30 +0700

    update

--- DailyNews/Screens/SearchController.swift
+++ DailyNews/Screens/SearchController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class SearchController: UIViewController {
     
+    @IBOutlet weak var buttonSubmit: UIButton!
+    
     @IBAction func submit(_ sender: Any) {
         let searchResultVC = self.storyboard?.instantiateViewController(withIdentifier: "SearchResultsController") as! SearchResultsController
         self.navigationController?.pushViewController(searchResultVC, animated: true)
@@ -22,6 +24,9 @@ class SearchController: UIViewController {
         self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
         self.navigationController?.navigationBar.barTintColor = UIColor(red: 0.89, green: 0.41, blue: 0.30, alpha: 1.00) // orange
         
+        buttonSubmit.backgroundColor = UIColor(red: 0.85, green: 0.51, blue: 0.42, alpha: 1.00)
+        buttonSubmit.layer.cornerRadius = 5
+        buttonSubmit.tintColor = UIColor.white
     }
     
 }
